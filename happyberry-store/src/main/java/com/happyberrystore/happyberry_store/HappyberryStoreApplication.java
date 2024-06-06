f3076570329c5e15dedd4704941c2d6cf7409e25
Jefferson56 <jemasapi56@gmail.com>
2024-06-06 15:14:17 -0500

    Configuración del proyecto, estructuración de carpetas

---
+++ happyberry-store/src/main/java/com/happyberrystore/happyberry_store/HappyberryStoreApplication.java
@@ -0,0 +1,13 @@
+package com.happyberrystore.happyberry_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyberryStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyberryStoreApplication.class, args);
+	}
+
+}
---
+++ happyberry-store/src/test/java/com/happyberrystore/happyberry_store/HappyberryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyberrystore.happyberry_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyberryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
